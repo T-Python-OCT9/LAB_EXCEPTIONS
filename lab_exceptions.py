e0f5fd937cfcc27d0e7a53157d9567f39b31c13e
Abdulmohsen Alateeq <mohsenalateeq@gmail.com>
2022-10-17 20:35:15 +0300

    Adding an error handler to the code.

---
+++ lab_exceptions.py
@@ -0,0 +1,15 @@
+# Below you have a code that raises an exception , using what you learned do the following:
+
+# Find what type of exception is raised.
+# NameError
+
+
+# Hanlde the exception in try..except
+try:
+    additoin(10, 20)
+except NameError:
+    # if operation fails, handle the specific exception that is raised , and print a relevant message.
+    print("There's an error in one of the variables name.")
+else:
+    # If operation successful , print "the operation is successful"
+    print("the operation is successful")
