23ef04f51b14dbb3170e5b3bb93c2744d3102945
saud <saud.almrwani@hotmail.com>
2022-10-18 02:05:30 +0300

    SAUD_EXCEPTIONS

---
+++ saud_Exceptions.py
@@ -0,0 +1,10 @@
+try : 
+    def additoin(x, y):
+          x = 10
+          y = 20
+          print("Addition:", x + b)
+except : ("the operation is successful")
+
+additoin(10, 20)
+
+raise NameError ("b is not defined")
