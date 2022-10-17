49a380436079208c292ae24f1f08b48dc52097d4
Faris A <Faris1d.sa@gmail.com>
2022-10-17 20:49:04 +0300

    Done ( :

---
+++ lab12.py
@@ -0,0 +1,23 @@
+
+def additoion(x , y):
+    x = 10
+    y = 20
+
+    print('additoion', x + b)
+
+
+
+
+
+
+try:
+    additoion(10, 20)
+except NameError: 
+    print('There is Error in the Names,Plase try to change one of names you chaoesed ')
+
+else:
+    print('the operation is successful')
+
+
+
+
