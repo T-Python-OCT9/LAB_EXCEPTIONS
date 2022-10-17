4b0976ccc862f89bd7ecc32fb6bf16af801b996d
ghadah <ghadahsabharbi@gmail.com>
2022-10-17 20:37:43 +0300

    exceptions lab -ghadah alhrbi

---
+++ excpetions.py
@@ -0,0 +1,20 @@
+# this is exceptions lab - 17 oct-ghadah alhrbi 
+
+
+
+
+try: 
+    def additoin(x, y):
+        x = 10
+        y = 20
+        print("Addition:", x + b)
+
+
+    additoin(10, 20)
+
+except NameError:
+    print("the b varible is not defined")
+
+else: 
+     print("the operation is successful")
+
